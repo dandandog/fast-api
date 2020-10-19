0a32c8824f1f3903d7161167340886a7a3a6ce5a
Johnny <935665309@qq.com>
2020-10-19 18:12:43 +0800

    first commit

---
+++ src/main/java/pers/dandandog/projects/ApiApplication.java
@@ -0,0 +1,25 @@
+package pers.dandandog.projects;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @author JohnnyLiu
+ */
+@SpringBootApplication
+@MapperScan(value = "pers.dandandog.projects", annotationClass = Mapper.class)
+public class ApiApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApiApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
